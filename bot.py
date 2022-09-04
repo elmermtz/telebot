968e1fa47669f571882f670097344b427f2084b1
Elmer Mtz <elmermtz@pop-os.localdomain>
2022-09-03 21:38:09 -0700

    editar mensaje de texto

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import time
 from config import *
 import telebot
 
@@ -9,12 +10,17 @@ bot = telebot.TeleBot(TELEGRAM_TOKEN)
 def cmd_start(message):
     bot.reply_to(message, "Hola!. Como andamios?")
 
+# responde a texto
 @bot.message_handler(content_types=["text"])
 def bot_mensajes_texto(message):
     if message.text.startswith("/"):
+        # envia al chat.id de donde vien el request el siguiente texto
         bot.send_message(message.chat.id, "comando no disponible")
+        
     else:
-        bot.send_message(message.chat.id, "sucribete a mi canal")
+        x = bot.send_message(message.chat.id, "<b>HOLA</b>", parse_mode="html", disable_web_page_preview=True)
+        time.sleep(3)
+        bot.edit_message_text("<u>ADIOS</u>", message.chat.id, x.message_id, parse_mode="html")
 
 
 # MAIN#####################################
