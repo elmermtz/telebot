28829e4c93b1eb94dd55b1fc76c0010a3fd7b934
Elmer Mtz <elmermtz@pop-os.localdomain>
2022-09-04 17:59:03 -0700

    recibe dos parametros foto y texto, responde si cumple requerimientos

--- bot.py
+++ bot.py
@@ -11,10 +11,11 @@ def cmd_start(message):
     bot.reply_to(message, "Hola!. Como andamios?")
 
 # ENVIAR DOCUMENTO
-@bot.message_handler(content_types=["text"])
+# aqui el type puede recibir textos y fotos
+@bot.message_handler(content_types=["text", "photo"])
 def bot_mensajes_texto(message):
-    # si el mensaje de texto empieza con /
-    if message.text.startswith("/"):
+    # si el mensaje cumple CON LAS DOS REQUERIMIENTOS entonces es TRUE, sino pasara a else
+    if message.text and message.text.startswith("/"):
         bot.send_message(message.chat.id, "comando no disponible")
     
 
@@ -24,6 +25,12 @@ def bot_mensajes_texto(message):
         archivo = open("./docs/lukas.odt", "rb")
         #  envia al chat.id de donde vien el request el siguiente archivo
         bot.send_document(message.chat.id, archivo, caption="DOCUMENTO")
+
+    # if message.photo:
+    #     bot.send_message(message.chat.id, "que linda photo")
+
+    # else:
+    #     pass
 
 
 # MAIN#####################################
